0930fbad28ca0a470a463fd0ffd5a0b3d37f9a83
chinocpu <ChinoCPU@protonmail.com>
2020-11-26 22:11:36 +0800

    update inst

--- src/inst_rom.v
+++ src/inst_rom.v
@@ -14,8 +14,12 @@ module inst_rom(
 		inst_mem[0] <= 64'h20808000000f0000;
 		inst_mem[1] <= 64'h2091000000002800;
 		inst_mem[2] <= 64'h2091880000001800;
-		inst_mem[3] <= 64'h20a2080000003000;
-		inst_mem[4] <= 64'h20a2800000002400;
+		inst_mem[3] <= 64'h1092006000000000;
+		inst_mem[4] <= 64'h10928c4000000000;
+		inst_mem[5] <= 64'h20a3080000003000;
+		inst_mem[6] <= 64'h20a3800000002400;
+		inst_mem[7] <= 64'h10a400c000000000;
+		inst_mem[8] <= 64'h10a498a000000000;
 		if (ce == `ChipDisable) begin
 			inst <= `ZeroDoubleWord;
 	  end else begin
