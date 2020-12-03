f4a56ae52555a288de71df4084442af1fc57356e
ChinoCPU <ChinoCPU@protonmail.com>
2020-12-03 09:38:17 +0800

    乘除指令测试代码

--- src/inst_rom.v
+++ src/inst_rom.v
@@ -11,8 +11,15 @@ module inst_rom(
 	reg[`InstBus]  inst_mem[0:`InstMemNum-1];
 
 	always @ (*) begin
-		inst_mem[0] <= 64'h2082000000320000;
-		inst_mem[1] <= 64'h20e27ffffff60000;
+inst_mem[0] <= 64'h2080800000050000;
+inst_mem[1] <= 64'h20f0800000028000;
+inst_mem[2] <= 64'h1081f80000000000;
+inst_mem[3] <= 64'h2082000000050000;
+inst_mem[4] <= 64'h20d2000000010000;
+inst_mem[5] <= 64'h1083780000000000;
+
+
+
 
 		if (ce == `ChipDisable) begin
 			inst <= `ZeroDoubleWord;
