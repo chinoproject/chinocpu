b51118fc9f3b149371f57ac4afb2e0524b4a5bcb
ChinoCPU <ChinoCPU@protonmail.com>
2021-02-12 20:12:51 +0800

    trap/syscall异常测试代码

---
+++ asm_src/trap_test.asm
@@ -0,0 +1,24 @@
+#trap异常测试
+_start:
+    jmp _main
+    nop
+
+    .org 0x40
+    mov $1,0x800
+    mov $1,0x900
+    mfc $14,$1
+    add $1,$1,0x8
+    mtc $14,$1
+    eret
+    nop
+
+.org 0x100
+_main:
+    mov $1,0x100
+    storew $1,$0,0
+    trap
+    loadw $2,$0,0
+
+_loop:
+    jmp _loop
+    nop
